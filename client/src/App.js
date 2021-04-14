904b9abcddb2e64e4fd87e98a30cbd2a326f12dc
julianactrl <juliana.mg3@gmail.com>
2021-04-14 10:45:12 -0300

    front

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Videogames</h1>
+      <>
+        <h1>Henry Videogames</h1>
+      </>
     </div>
   );
 }
