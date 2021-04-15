d570cade8ecb3de89961e20d6b2c69aace15f33b
julianactrl <juliana.mg3@gmail.com>
2021-04-15 09:26:12 -0300

    home listo

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
-import 'styles'
+import './styles.css'
+import videogame from './../../assets/videogame.png';
+
 const Home = () => (
     <div className="container">
       <div className="main">
+        <img src={videogame} alt="portada"/>
         <h2>Henry Videogames</h2>
         <Link to="/inicio">
           <button type="submit">LEST ´S GO PLAY</button>
