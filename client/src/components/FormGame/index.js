43cc0580045b02a0a532bffbf9a68dd761fbe59f
julianactrl <juliana.mg3@gmail.com>
2021-04-20 18:24:42 -0300

    form game

--- client/src/components/FormGame/index.js
+++ client/src/components/FormGame/index.js
@@ -1,55 +1,130 @@
 import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { getGenresDb } from "../../redux/actions";
+import { getGenresDb, postNewGame } from "../../redux/actions";
 import "./styles.css";
+import swal from "sweetalert";
 
 const FormGame = () => {
-  const dispatch = useDispatch();
-  const genres = useSelector((state) => state.genresState.genres);
-  useEffect(() => {
-    dispatch(getGenresDb());
-  }, []);
+  const history = useHistory()
 
-  // name, description, released, rating, platforms, genres
+  const initGame = {
+    name: "",
+    description: "",
+    released: "",
+    rating: 0,
+    genres: [],
+    platforms: "",
+  }
+  const [game, setGame] = useState(initGame)
+
+  const dispatch = useDispatch()
+  const genresRedux = useSelector((state) => state.genresState.genres)
+
+  useEffect(() => {
+    dispatch(getGenresDb())
+  }, [])
+
+  const gameDb = (e) => {
+    e.preventDefault()
+    const gameSend = {
+      name: game.name,
+      description: game.description,
+      released: game.released,
+      rating: game.rating,
+      genres: game.genres,
+      platforms: game.platforms,
+    }
+    dispatch(postNewGame(gameSend))
+    e.target.reset();
+    swal("Game created succesfully!", {
+      buttons: false,
+      timer: 1000,
+    })
+  }
+
+  const handleInputChange = (e) => {
+    if (e.target.name === "genres") {
+      const arr = game[e.target.name];
+      setGame({
+        ...game,
+        [e.target.name]: arr.concat(e.target.value),
+      });
+    } else {
+      setGame({
+        ...game,
+        [e.target.name]: e.target.value,
+      })
+    }
+  }  
+
   return (
-    <div class="container_form">
-      <form id="form_game" action="" method="post">
-        <h3>Create your own video game</h3>
+    <div className="container_form">
+      <form id="form_game" onSubmit={(e) => gameDb(e)}  onChange={(e) => handleInputChange(e)}>
+        <h3>Create your video game</h3>
         <h4>Use your imagination</h4>
         <fieldset>
           <input
+            value={game.name}
+            name="name"
             placeholder="Give it a name"
             type="text"
-            tabindex="1"
+            tabIndex="1"
             required
-            autofocus
+            autoFocus
           />
         </fieldset>
         <fieldset>
           <textarea
+            value={game.description}
+            name="description"
+            type="text"
             placeholder="Tell us what about is it..."
-            tabindex="5"
+            tabIndex="5"
             required
           ></textarea>
         </fieldset>
         <fieldset>
-          <input placeholder="Released" type="date" tabindex="3" required />
+          <input
+            value={game.released}
+            name="released"
+            placeholder="Released"
+            type="date"
+            tabIndex="3"
+            required
+          />
         </fieldset>
         <fieldset>
-          <input placeholder="Rating" type="number" tabindex="4" required />
+          <input
+            value={game.rating}
+            name="rating"
+            placeholder="Rating"
+            type="number"
+            tabIndex="4"
+            required
+          />
         </fieldset>
         <fieldset>
-          <textarea
+          <input
+            value={game.platforms}
+            name="platforms"
             placeholder="What Platforms are going hold"
-            tabindex="5"
+            type="text"
+            tabIndex="5"
             required
-          ></textarea>
+          ></input>
         </fieldset>
-        <fieldset>
+        <fieldset className="select_field">
           <label>Select the genres for your game</label>
-          <select multiple>
-            {genres.map((g, id) => (
-              <option key={id}>{g.name}</option>
+          <select className="select_genres" multiple={true}>
+            {genresRedux && genresRedux.map((g) => (
+              <option
+                value={g.name}
+                name="genres"  
+                key={g.id}
+              >
+                {g.name}
+              </option>
             ))}
           </select>
         </fieldset>
@@ -59,6 +134,9 @@ const FormGame = () => {
             type="submit"
             id="contact-submit"
             data-submit="...Sending"
+            onClick={() => {
+              history.goBack();
+            }}
           >
             Submit
           </button>
