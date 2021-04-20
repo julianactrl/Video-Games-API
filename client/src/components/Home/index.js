51d40275181fe905050fff529c641b9d0b72b8d7
julianactrl <juliana.mg3@gmail.com>
2021-04-20 01:21:17 -0300

    arreglo de estilos

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -5,7 +5,7 @@ import videogame from './../../assets/videogame.png';
 const Home = () => (
     <div className="container">
         <img src={videogame} alt="portada"/>
-        <h2>Henry Videogames</h2>
+        <h2 className="init_title">Henry Videogames</h2>
         <Link to="/init">
           <button className="button" type="submit">LEST ´S GO PLAY</button>
         </Link>
